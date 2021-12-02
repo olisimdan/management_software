99148cbadd97337c9bcfc5f3817d5113ad39f97f
Daniel Qvistgaard <danielqvistgaard@outlook.com>
2021-12-02 19:46:38 +0100

    Added main python file

---
+++ main.py
@@ -0,0 +1,37 @@
+import datetime
+
+
+
+
+
+
+
+class Task:
+    def __init__(self) -> None:
+        self.identifier
+        self.title
+        self.description 
+        self.date_added
+
+    def edit(self):
+        pass
+
+    def create(self):
+        pass
+
+    def finish(self):
+        pass
+
+
+class Tasklist:
+    def __init__(self) -> None:
+        pass
+    
+
+def main():
+    pass        
+
+
+
+if __name__ == "__main__":
+    main()
